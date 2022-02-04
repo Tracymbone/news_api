b2ba807269cd48e61ead68ea65ffb42fffb49df4
moringa <tressy.mbone@student.moringaschool.com>
2022-02-04 20:13:19 +0300

    deploy to heroku

--- run.py
+++ run.py
@@ -1,28 +1,74 @@
-# from . import app
-
 from flask import Flask, render_template
-import requests
-import urllib.request,json
+
+from newsapi import NewsApiClient
 
 
-app=Flask(__name__)
+app = Flask(__name__)
+API_KEY='45778e35f22c47dfadc524155cee34d6'
+
 @app.route('/')
-def home_page():
-    base_url="https://newsapi.org/v2/everything?q=Apple&from=2022-01-25&sortBy=popularity&apiKey=45778e35f22c47dfadc524155cee34d6"
-    data=requests.get(base_url).json()["articles"]
-    
-    return render_template('index.html',news=data)
-    
-@app.route('/headlines')
-def headlines():
-    base_url="https://newsapi.org/v2/headlines?q=Apple&from=2022-01-25&sortBy=popularity&apiKey=45778e35f22c47dfadc524155cee34d6"
-    data=requests.get(base_url).json()["articles"]
-      
-    
-    return render_template('headlines.html',news=data)
-    
-    
+def index():
+    newapi = NewsApiClient(API_KEY)
+    topheadlines = newapi.get_top_headlines(sources="bbc-news,al-jazeera-english")
 
- 
-if __name__ == '__main__':
-    app.run(debug=True)
+    articles = topheadlines['articles']
+
+    news = []
+    description = []
+    link = []
+    img = []
+    time = []
+    content = []
+   
+
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        link.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+        time.append(myarticles['publishedAt'])
+        content.append(myarticles['content'])
+       
+
+
+        my_list = zip( news,description,link,img,time,content)
+        return render_template('index.html', context=my_list)
+    
+    @app.route('/alja')
+    def home():
+     newapi = NewsApiClient(API_KEY)
+    topheadlines = newapi.get_top_headlines(sources="al-jazeera-english")
+
+    articles = topheadlines['articles']
+
+    news = []
+    description = []
+    link = []
+    img = []
+    time = []
+    content = []
+   
+
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        link.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+        time.append(myarticles['publishedAt'])
+        content.append(myarticles['content'])
+       
+
+
+        my_list = zip( news,description,link,img,time,content )
+        return render_template('headlines.html', context=my_list)
+    
+    
+    
+    if __name__ == '__main__':
+     app.run(debug = True) 
+        
+        
